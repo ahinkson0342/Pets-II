831ac54a867bb5d4b744215b3a8c84cc3af19171
Andrew Hinkson <adh1056@comcast.net>
2024-04-27 23:42:44 -0400

    finalized website functionality

--- index.php
+++ index.php
@@ -1,38 +1,36 @@
-<!-----------------------Github repo: https://github.com/ahinkson0342/Pets-II---------------------->
 <?php
+session_start();
 
-//turning on error reporting
+// 328/diner/index.php
+// This is my CONTROLLER!
+
+// Turn on error reporting
 ini_set('display_errors', 1);
 error_reporting(E_ALL);
 
-//Require autoload file
-require_once('vendor/autoload.php');
+// Require the autoload file
+require_once ('vendor/autoload.php');
 
-//Create instance of base class
+// Instantiate the F3 Base class
 $f3 = Base::instance();
 
-//Define a default route
-$f3->route('GET /',function()
-{
-    //echo '<h1>Hello World!</h1>';
+// Define a default route
+// https://tostrander.greenriverdev.com/328/hello-fat-free/
+$f3->route('GET /', function() {
 
-    //Render a view page
+    // Render a view page
     $view = new Template();
     echo $view->render('views/home.html');
 });
 
-$f3->route('GET /home',function()
-{
-    //echo '<h1>Hello World!</h1>';
+$f3->route('GET /home', function() {
 
-    //Render a view page
+    // Render a view page
     $view = new Template();
     echo $view->render('views/home.html');
 });
 
-$f3->route('GET /order',function()
-{
-    //echo '<h1>Hello World!</h1>';
+$f3->route('GET|POST /order', function($f3) {
 
     // If the form has been posted
     if ($_SERVER['REQUEST_METHOD'] == "POST") {
@@ -45,14 +43,14 @@ $f3->route('GET /order',function()
         $petColor = $_POST['petColor'];
 
         // If the data valid
-        if (isset($petType) && isset($petColor)) {
+        if (!empty($petType)) {
 
             // Add the data to the session array
             $f3->set('SESSION.petType', $petType);
             $f3->set('SESSION.petColor', $petColor);
 
             // Send the user to the next form
-            $f3->reroute('next');
+            $f3->reroute('summary');
         }
         else {
             // Temporary
@@ -61,10 +59,18 @@ $f3->route('GET /order',function()
         }
     }
 
-    //Render a view page
+    // Render a view page
     $view = new Template();
     echo $view->render('views/pet-order.html');
 });
 
-//Run fat-free
-$f3->Run();
+$f3->route('GET|POST /summary', function($f3) {
+
+
+    // Render a view page
+    $view = new Template();
+    echo $view->render('views/summary.html');
+});
+
+// Run Fat-Free
+$f3->run();
