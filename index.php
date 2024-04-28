ae3b7d1f3e1ab0bccb14e48813996910f70d3ac3
Andrew Hinkson <adh1056@comcast.net>
2024-04-27 23:19:59 -0400

    made progress on order page

--- index.php
+++ index.php
@@ -34,9 +34,36 @@ $f3->route('GET /order',function()
 {
     //echo '<h1>Hello World!</h1>';
 
+    // If the form has been posted
+    if ($_SERVER['REQUEST_METHOD'] == "POST") {
+
+        //echo "<p>You got here using the POST method</p>";
+        //var_dump ($_POST);
+
+        // Get the data from the post array
+        $petType = $_POST['petType'];
+        $petColor = $_POST['petColor'];
+
+        // If the data valid
+        if (isset($petType) && isset($petColor)) {
+
+            // Add the data to the session array
+            $f3->set('SESSION.petType', $petType);
+            $f3->set('SESSION.petColor', $petColor);
+
+            // Send the user to the next form
+            $f3->reroute('next');
+        }
+        else {
+            // Temporary
+            echo "<p class='text-center text-danger'>Please ensure you filled out all fields!</p>";
+
+        }
+    }
+
     //Render a view page
     $view = new Template();
-    echo $view->render('views/order.html');
+    echo $view->render('views/pet-order.html');
 });
 
 //Run fat-free
