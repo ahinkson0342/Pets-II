a97f05f51f3c4163bb509fa04324c0f5cb8b01aa
Andrew Hinkson <adh1056@comcast.net>
2024-05-21 22:49:02 -0400

    added roboticPet class

--- classes/roboticPet.php
+++ classes/roboticPet.php
@@ -1,1 +1,27 @@
 <?php
+
+class RoboticPet extends Pet
+{
+    private $_accessories = [];
+
+    function addAcc($acc)
+    {
+        $this->_accessories[] = $acc;
+    }
+
+    function removeAcc($acc)
+    {
+        for($i = 0; $i < sizeOf($this->_accessories); $i++)
+        {
+            if(strcmp($acc, $this->_accessories))
+            {
+                array_splice($acc, $i, 1);
+            }
+        }
+    }
+
+    function charge()
+    {
+        echo "<p>Robotic pet is charging...</p>";
+    }
+}
