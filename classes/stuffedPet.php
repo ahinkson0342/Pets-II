679cf0ceefa6d5ee899a03f38285648d86374d34
cmatthews444 <155667279+cmatthews444@users.noreply.github.com>
2024-05-21 19:36:27 -0700

    Added stuffedPet.php and constructors

--- classes/stuffedPet.php
+++ classes/stuffedPet.php
@@ -1,1 +1,42 @@
 <?php
+class StuffedPet extends Pet {
+    private $_size;
+    private $_stuffingType;
+    private $_material;
+
+
+    function __construct($animal = "unknown", $color = "?", $size = "medium", $stuffingType = "cotton", $material = "fabric") {
+        parent::__construct($animal, $color);
+        $this->_size = $size;
+        $this->_stuffingType = $stuffingType;
+        $this->_material = $material;
+    }
+    function setSize($size) {
+        $this ->_size = $size;
+    }
+    function getSize() {
+        return $this->_size;
+    }
+
+
+    function setStuffingType($stuffingType) {
+        $this->_stuffingType = $stuffingType;
+    }
+
+
+    function getStuffingType() {
+        return $this->_stuffingType;
+    }
+
+
+    function setMaterial($material) {
+        $this->_material = $material;
+    }
+
+
+    function getMaterial() {
+        return $this->_material;
+    }
+
+}
+
