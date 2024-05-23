bc024484e7f15f996c87c3a19da4abf3355052ec
Andrew Hinkson <adh1056@comcast.net>
2024-05-23 15:36:23 -0400

    all pages accessible through controller now

--- index.php
+++ index.php
@@ -13,63 +13,28 @@ require_once ('vendor/autoload.php');
 
 // Instantiate the F3 Base class
 $f3 = Base::instance();
+$con = new Controller($f3);
 
 // Define a default route
 // https://tostrander.greenriverdev.com/328/hello-fat-free/
 $f3->route('GET /', function() {
-
-    // Render a view page
-    $view = new Template();
-    echo $view->render('views/home.html');
+    $GLOBALS['con']->home();
 });
 
 $f3->route('GET /home', function() {
-
-    // Render a view page
-    $view = new Template();
-    echo $view->render('views/home.html');
+    $GLOBALS['con']->home();
 });
 
-$f3->route('GET|POST /order', function($f3) {
-
-    // If the form has been posted
-    if ($_SERVER['REQUEST_METHOD'] == "POST") {
-
-        //echo "<p>You got here using the POST method</p>";
-        //var_dump ($_POST);
-
-        // Get the data from the post array
-        $petType = $_POST['petType'];
-        $petColor = $_POST['petColor'];
-
-        // If the data valid
-        if (!empty($petType)) {
-
-            // Add the data to the session array
-            $f3->set('SESSION.petType', $petType);
-            $f3->set('SESSION.petColor', $petColor);
-
-            // Send the user to the next form
-            $f3->reroute('summary');
-        }
-        else {
-            // Temporary
-            echo "<p class='text-center text-danger'>Please ensure you filled out all fields!</p>";
-
-        }
-    }
-
-    // Render a view page
-    $view = new Template();
-    echo $view->render('views/pet-order.html');
+$f3->route('GET /order', function() {
+    $GLOBALS['con']->order();
 });
 
-$f3->route('GET|POST /summary', function($f3) {
+$f3->route('GET /RoboticPet', function() {
+    $GLOBALS['con']->roboticPet();
+});
 
-
-    // Render a view page
-    $view = new Template();
-    echo $view->render('views/summary.html');
+$f3->route('GET /StuffedPet', function() {
+    $GLOBALS['con']->stuffedPet();
 });
 
 // Run Fat-Free
